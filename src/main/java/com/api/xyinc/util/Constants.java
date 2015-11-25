04287dd7a7973473ab8626d3706505a0629151ce
Rafael Faria <rafaelf.fmg@gmail.com>
2015-11-25 10:26:45 -0200

    Ajustes

--- src/main/java/com/api/xyinc/util/Constants.java
+++ src/main/java/com/api/xyinc/util/Constants.java
@@ -1,6 +1,8 @@
 package com.api.xyinc.util;
 
 public class Constants {
+
     public static final String URL_CORREIOS = "http://www.buscacep.correios.com.br";
     public static final String ACTION_CORREIOS = "/servicos/dnec/consultaEnderecoAction.do";
+
 }
